5eae0ee2809a02daa2d2a526c57b6157051e2886
Hannah <hannah.costa8406@gmail.com>
2019-10-29 18:52:29 -0300

    Started AVL

--- AVLTree.py
+++ AVLTree.py
@@ -1,4 +1,58 @@
+class Node(object):
+	def __init__(self, data = None):
+		self.data = data
+		self.left = None
+		self.right = None
+		self.balance = 0 # balance factor of the node
+
+		
 class AVLTree(object):
 	def __init__(self):
-		self.arg = arg
+		self.root = None
+
+
+	def calculateBF(self):
+		
+
+
+	def balance(self, node):
+		# TODO!!!
+
+
+	def leftRotation(self):
+
+
+
+	def rightRotation(self):
+
+
+
+	def leftRightRotation(self):
+
+
+
+	def rightLeftRotation(self):
+
+
+
+
+	def insert(self, data):
+		self.root = self._insert(data, self.root)
+
+
+	def _insert(self, data, node):
+
+		# Recursive insertion.
+		# TODO: after inserting, balance the tree
+
+		if node is None:
+			node = Node(data)
+		else:
+			stat = self._compare(data, node.data)
+			if stat < 0:
+				node.left = self._insert(data, node.left)
+			else:
+				node.right = self._insert(data, node.right)
+
+		return node
 		
